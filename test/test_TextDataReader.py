227fa7a3852b9d67aab97bae59816f8446e625a0
Kholodov_iv <Kholodov_iv@gmail.com>
2025-10-30 07:51:50 +0300

    Second fix PEP8 style in test_TextDataReader.py

--- test/test_TextDataReader.py
+++ test/test_TextDataReader.py
@@ -1,41 +1,41 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import pytest
 from pathlib import Path
-from Types import DataType 
-from TextDataReader import TextDataReader 
+from Types import DataType
+from TextDataReader import TextDataReader
 
 
-class TestTextDataReader: 
+class TestTextDataReader:
 
-    @pytest.fixture() 
-    def file_and_data_content(self) -> tuple[str, DataType]: 
-        text = ("Иванов Константин Дмитриевич\n" + 
-                "    математика:91\n" + 
-                "    химия:100\n" + 
-                "Петров Петр Семенович\n" + 
-                "    русский язык:87\n" + 
+    @pytest.fixture()
+    def file_and_data_content(self) -> tuple[str, DataType]:
+        text = ("Иванов Константин Дмитриевич\n" +
+                "    математика:91\n" +
+                "    химия:100\n" +
+                "Петров Петр Семенович\n" +
+                "    русский язык:87\n" +
                 "    литература:78\n")
- 
-        data = { 
-            "Иванов Константин Дмитриевич": [ 
-                ("математика", 91), ("химия", 100) 
-            ], 
-            "Петров Петр Семенович": [ 
-                ("русский язык", 87), ("литература", 78) 
-            ] 
-        } 
-        return text, data 
 
-    @pytest.fixture() 
-    def filepath_and_data(self, 
-                          file_and_data_content: tuple[str, DataType], 
-                          tmpdir) -> tuple[str, DataType]: 
+        data = {
+            "Иванов Константин Дмитриевич": [
+                ("математика", 91), ("химия", 100)
+            ],
+            "Петров Петр Семенович": [
+                ("русский язык", 87), ("литература", 78)
+            ]
+        }
+        return text, data
+
+    @pytest.fixture()
+    def filepath_and_data(self,
+                          file_and_data_content: tuple[str, DataType],
+                          tmpdir) -> tuple[str, DataType]:
         p = Path(tmpdir) / "datadir" / "my_data.txt"
         p.parent.mkdir(parents=True, exist_ok=True)
         # Записываем с кодировкой UTF-8
         p.write_text(file_and_data_content[0], encoding='utf-8')
         return str(p), file_and_data_content[1]
 
-    def test_read(self, filepath_and_data: tuple[str, DataType]) -> None: 
-        file_content = TextDataReader().read(filepath_and_data[0]) 
+    def test_read(self, filepath_and_data: tuple[str, DataType]) -> None:
+        file_content = TextDataReader().read(filepath_and_data[0])
         assert file_content == filepath_and_data[1]
